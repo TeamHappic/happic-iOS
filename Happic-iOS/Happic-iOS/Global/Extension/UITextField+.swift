ce94a0bd00fa4af63602fc5c8e4013fead607964
Sejin Lee <lsj8706@naver.com>
2022-07-09 02:44:31 +0900

    [Add] 기초 Extension들 추가

---
+++ Happic-iOS/Happic-iOS/Global/Extension/UITextField+.swift
@@ -0,0 +1,24 @@
+//
+//  UITextField+.swift
+//  Happic-iOS
+//
+//  Created by sejin on 2022/07/09.
+//
+
+import UIKit
+
+extension UITextField {
+    /// UITextField 왼쪽에 여백 주는 메서드
+    func addLeftPadding(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+
+    /// UITextField 오른쪽에 여백 주는 메서드
+    func addRightPadding(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
---
+++ Happic-iOS/Happic-iOS/Global/Extension/UserDefaults+.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaults+.swift
+//  Happic-iOS
+//
+//  Created by sejin on 2022/07/09.
+//
+
+import Foundation
+
+extension UserDefaults {
+    /// UserDefaults key value가 많아지면 관리하기 어려워지므로 enum 'Keys'로 묶어 관리합니다.
+    enum Keys {
+        /// String
+        static var PW = "PW"
+    }
+}
