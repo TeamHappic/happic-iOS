a2e3a9863b55070e5e3e110cd59bbbd667517483
Sejin Lee <lsj8706@naver.com>
2022-07-11 16:15:19 +0900

    [Feat] 스크롤 뷰 추가

--- Happic-iOS/Happic-iOS/Screens/HappicReport/Controller/HappicReportController.swift
+++ Happic-iOS/Happic-iOS/Screens/HappicReport/Controller/HappicReportController.swift
@@ -6,9 +6,36 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class HappicReportController: UIViewController {
+    
+    // MARK: - UI
+    private let reportScrollView = UIScrollView()
+    private let contentView = UIView()
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Functions
+    private func configureUI() {
+        setScrollViewLayout()
+    }
+    
+    private func setScrollViewLayout() {
+        view.addSubviews(reportScrollView, contentView)
+        reportScrollView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.edges.equalTo(reportScrollView.contentLayoutGuide)
+            make.width.equalTo(reportScrollView.snp.width)
+            make.height.greaterThanOrEqualTo(reportScrollView.snp.height).priority(500)
+        }
     }
 }
