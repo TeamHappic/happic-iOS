44ce583fd61f92af5e1d12605ec5909f449b8661
Sejin Lee <lsj8706@naver.com>
2022-07-11 13:13:55 +0900

    [Feat] Main.storyboard 삭제 및 루트 뷰컨트롤러 변경

--- Happic-iOS/Happic-iOS/TabBarController.swift
+++ Happic-iOS/Happic-iOS/TabBarController.swift
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        // Do any additional setup after loading the view.        
     }
 }
