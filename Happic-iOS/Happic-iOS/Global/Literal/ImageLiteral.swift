ea516252032f75bd2909aa2c736d2f94d97e7164
devxsby <devxsby@gmail.com>
2022-07-13 00:11:24 +0900

    [Feat] Image Literal 추가

--- Happic-iOS/Happic-iOS/Global/Literal/ImageLiteral.swift
+++ Happic-iOS/Happic-iOS/Global/Literal/ImageLiteral.swift
@@ -9,6 +9,12 @@ import UIKit
 
 enum ImageLiteral {
     static var icnAddPhoto: UIImage { .load(named: "icn_addphoto")}
+    static var icnArrowMore: UIImage { .load(named: "icn_arrow_more")}
+    
+    static var imageDailySample1: UIImage { .load(named: "image_daily_sample1")}
+    static var imageDailySample2: UIImage { .load(named: "image_daily_sample2")}
+    static var imageDailySample3: UIImage { .load(named: "image_daily_sample3")}
+    static var imageDailySample4: UIImage { .load(named: "image_daily_sample4")}
 }
 
 extension UIImage {
