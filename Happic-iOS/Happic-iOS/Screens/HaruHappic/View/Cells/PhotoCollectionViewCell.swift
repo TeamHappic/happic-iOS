276b60b11bd091e29033094596ae534692199053
devxsby <devxsby@gmail.com>
2022-07-13 00:20:33 +0900

    [Feat] 하루 해픽 포토 컬렉션 뷰 셀 구현

---
+++ Happic-iOS/Happic-iOS/Screens/HaruHappic/View/Cells/PhotoCollectionViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Happic-iOS
+//
+//  Created by devxsby on 2022/07/12.
+//
+
+import UIKit
+
+final class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - UI
+    private lazy var dailyImage = UIImageView().then {
+        $0.image = ImageLiteral.imageDailySample1
+        $0.layer.cornerRadius = 8
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+        $0.backgroundColor = .black
+    }
+    
+    private lazy var dailyLabel = UILabel().then {
+        $0.text = "1"
+        $0.textColor = .hpWhite
+        $0.font = UIFont.font(.pretendardMedium, ofSize: 12)
+    }
+    
+    // MARK: - View Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    private func configureUI() {
+        
+        addSubviews(dailyLabel, dailyImage)
+        
+        dailyImage.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(self.frame.width)
+        }
+        
+        dailyLabel.snp.makeConstraints { make in
+            make.top.equalTo(dailyImage.snp.bottom).offset(5)
+            make.centerX.equalToSuperview()
+        }
+    }
+}
