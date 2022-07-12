1dfb0927e8e24d95511e166e5845b91399796e65
Sejin Lee <lsj8706@naver.com>
2022-07-12 17:26:26 +0900

    [Feat] CategoryDetailWithImageCollectionViewCell 생성

---
+++ Happic-iOS/Happic-iOS/Screens/HappicReport/View/Cell/CategoryDetailWithImageCollectionViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  CategoryDetailWithImageCollectionViewCell.swift
+//  Happic-iOS
+//
+//  Created by sejin on 2022/07/12.
+//
+
+import UIKit
+
+class CategoryDetailWithImageCollectionViewCell: UICollectionViewCell {
+ 
+    // MARK: - UI
+    private lazy var categoryNameLabel = UILabel().then {
+        $0.text = "#who 1"
+        $0.textColor = .gray
+        $0.font = UIFont.font(.pretendardRegular, ofSize: 14)
+    }
+    
+    private lazy var userKeywordLabel = UILabel().then {
+        $0.text = "수빈"
+        $0.textColor = .white
+        $0.font = UIFont.font(.pretendardBold, ofSize: 16)
+    }
+    
+    private lazy var countLabel = UILabel().then {
+        $0.text = "xx회"
+        $0.textColor = .white
+        $0.font = UIFont.font(.pretendardMedium, ofSize: 14)
+    }
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    private func configureUI() {
+        addSubviews(categoryNameLabel, userKeywordLabel, countLabel)
+        
+        categoryNameLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(24)
+            make.top.equalToSuperview().inset(18)
+        }
+        
+        userKeywordLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(94)
+            make.centerY.equalTo(categoryNameLabel)
+        }
+        
+        countLabel.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(42)
+            make.centerY.equalTo(categoryNameLabel)
+        }
+    }
+}
