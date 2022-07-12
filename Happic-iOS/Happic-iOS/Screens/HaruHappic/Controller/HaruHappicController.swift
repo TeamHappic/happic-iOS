191c9bb8dd777ab65f6e8261b35c8c69184a8c11
devxsby <devxsby@gmail.com>
2022-07-12 16:05:09 +0900

    [Feat] add photo 버튼 추가

--- Happic-iOS/Happic-iOS/Screens/HaruHappic/Controller/HaruHappicController.swift
+++ Happic-iOS/Happic-iOS/Screens/HaruHappic/Controller/HaruHappicController.swift
@@ -8,7 +8,36 @@
 import UIKit
 
 final class HaruHappicController: UIViewController {
+    
+    // MARK: - UI
+    private lazy var addPhotoButton = UIButton(type: .system).then {
+        $0.setImage(ImageLiteral.icnAddPhoto, for: .normal)
+        $0.tintColor = .white
+    }
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        setCollectionView()
+        setCollectionViewLayout()
+    }
+    
+    // MARK: - Functions
+    private func configureUI() {
+        view.addSubview(addPhotoButton)
+        addPhotoButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(30)
+            make.trailing.equalToSuperview().inset(30)
+            make.width.height.equalTo(20)
+        }
+    }
+    
+    private func setCollectionView() {
+        
+    }
+    
+    private func setCollectionViewLayout() {
+        
     }
 }
