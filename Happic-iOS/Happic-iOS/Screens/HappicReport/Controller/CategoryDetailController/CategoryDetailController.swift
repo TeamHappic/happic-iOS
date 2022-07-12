ace1eab541ce58b6a8c8729d6a6525f99b7f4beb
Sejin Lee <lsj8706@naver.com>
2022-07-12 16:09:47 +0900

    [Chore] 카테고리 디테일 뷰컨트롤러들을 하나로 일원화

---
+++ Happic-iOS/Happic-iOS/Screens/HappicReport/Controller/CategoryDetailController/CategoryDetailController.swift
@@ -0,0 +1,18 @@
+//
+//  HourCategoryDetailController.swift
+//  Happic-iOS
+//
+//  Created by sejin on 2022/07/12.
+//
+
+import UIKit
+
+class CategoryDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .gray
+    }
+
+}
--- Happic-iOS/Happic-iOS/Screens/HappicReport/View/CategoryRankView.swift
+++ Happic-iOS/Happic-iOS/Screens/HappicReport/View/CategoryRankView.swift
@@ -10,6 +10,15 @@ import UIKit
 class CategoryRankView: UIView {
     // MARK: - UI
     private lazy var headerView = HappicReportSectionHeader(type: .categoryRank)
+    // ViewPager에 들어갈 카테고리 별 ViewController 처리
+    private lazy var hourViewController = CategoryDetailController()
+    private lazy var whereViewController = CategoryDetailController()
+    private lazy var whoViewController = CategoryDetailController()
+    private lazy var whatViewController = CategoryDetailController()
+    
+    private lazy var categoryViewPager = CustomViewPager(
+        viewControllers: [hourViewController, whereViewController, whoViewController, whatViewController],
+        buttonTitles: ["#hour", "#where", "#who", "#what"], isScrollEnabled: false)
 
     // MARK: - View Life Cycle
     override init(frame: CGRect) {
@@ -24,9 +33,15 @@ class CategoryRankView: UIView {
     
     // MARK: - Functions
     private func configureUI() {
-        addSubviews(headerView)
+        addSubviews(headerView, categoryViewPager)
         headerView.snp.makeConstraints { make in
             make.leading.top.trailing.equalToSuperview()
+        }
+        
+        categoryViewPager.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(32)
         }
     }
 }
