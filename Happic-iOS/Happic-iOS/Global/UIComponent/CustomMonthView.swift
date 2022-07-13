0aadf44a0c52262a19f6ff9d7c6ea57c0ccd7c8c
devxsby <devxsby@gmail.com>
2022-07-13 14:20:47 +0900

    [Add] back, next 아이콘 이미지 에셋 추가

---
+++ Happic-iOS/Happic-iOS/Global/UIComponent/CustomMonthView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomMonthView.swift
+//  Happic-iOS
+//
+//  Created by devxsby on 2022/07/13.
+//
+
+import UIKit
+
+class CustomMonthView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
