6c70db79fd6e12605fcb406d114c7a273ff46158
devxsby <devxsby@gmail.com>
2022-07-13 15:23:36 +0900

    [Add] Custom Month Picker View 파일 추가

---
+++ Happic-iOS/Happic-iOS/Global/UIComponent/CustomMonthPickerView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomMonthPickerView.swift
+//  Happic-iOS
+//
+//  Created by devxsby on 2022/07/13.
+//
+
+import UIKit
+
+class CustomMonthPickerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
