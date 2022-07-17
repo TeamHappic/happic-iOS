20632d829d8107637239006bb22f9fa7dc262c37
devxsby <devxsby@gmail.com>
2022-07-17 15:55:29 +0900

    [Feat] BaseUploadViewController 구현
    홈 뷰와 하루해픽 뷰에서 상속받아 사용 가능합니다.

--- Happic-iOS/Happic-iOS/Global/Base/BaseUploadViewController.swift
+++ Happic-iOS/Happic-iOS/Global/Base/BaseUploadViewController.swift
@@ -5,4 +5,65 @@
 //  Created by devxsby on 2022/07/17.
 //
 
-import Foundation
+import UIKit
+
+class BaseUploadViewController: UIViewController {
+    
+    let picker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setPickerView()
+    }
+    
+    func setActionSheet() {
+        let actionSheet = UIAlertController(title: "사진을 추가하세요", message: nil, preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "카메라", style: .default) { _ in
+            self.openCamera()
+        }
+        
+        let photoLibraryAction = UIAlertAction(title: "갤러리", style: .default) { _ in
+            self.openPhotoLibrary()
+        }
+        
+        let cancelAction = UIAlertAction(title: "닫기", style: .cancel, handler: nil)
+        
+        actionSheet.addAction(cameraAction)
+        actionSheet.addAction(photoLibraryAction)
+        actionSheet.addAction(cancelAction)
+        
+        self.present(actionSheet, animated: true)
+    }
+}
+
+extension BaseUploadViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func setPickerView() {
+        picker.delegate = self
+    }
+    
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+        } else {
+            print("Camera not available")
+        }
+    }
+    
+    func openPhotoLibrary() {
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: false, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        let createContentsController = CreateContentsController()
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            createContentsController.pickerImageView.image = image
+            picker.dismiss(animated: true, completion: nil)
+            createContentsController.modalPresentationStyle = .fullScreen
+            present(createContentsController, animated: true, completion: nil)
+        }
+    }
+}
