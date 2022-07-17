13ea39389eafaa0ee780115c8e8ba2e4fe66e9d4
Sejin Lee <lsj8706@naver.com>
2022-07-18 03:45:21 +0900

    [Docs] 주석 공백 제거

--- Happic-iOS/Happic-iOS/Network/Foundation/NetworkResult.swift
+++ Happic-iOS/Happic-iOS/Network/Foundation/NetworkResult.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum NetworkResult<T> {
-    case success(T) //서버 통신 성공했을 때
+    case success(T) // 서버 통신 성공했을 때
     case requestErr(T) // 요청 에러 발생했을 때
     case pathErr(T) // 경로 에러 발생했을 때
     case serverErr // 서버의 내부 에러가 발생했을 때
