5eea334ad4faa46b122d8cb63077497f2c0950f1
devxsby <devxsby@gmail.com>
2022-07-18 00:27:09 +0900

    [Chore] 탭바 아이콘 변경

--- Happic-iOS/Happic-iOS/Screens/Tabbar/Controller/TabBarController.swift
+++ Happic-iOS/Happic-iOS/Screens/Tabbar/Controller/TabBarController.swift
@@ -25,28 +25,28 @@ final class TabBarController: UITabBarController {
         view.backgroundColor = .black
         
         let homeController = templateNavigationController(title: "홈",
-                                                          unselectedImage: UIImage(systemName: "house")!,
-                                                          selectedImage: UIImage(systemName: "house")!,
+                                                          unselectedImage: ImageLiterals.icnHomeOff,
+                                                          selectedImage: ImageLiterals.icnHomeOn,
                                                           rootViewController: HomeController())
         
         let haruHappicController = templateNavigationController(title: "하루해픽",
-                                                                unselectedImage: UIImage(systemName: "calendar")!,
-                                                                selectedImage: UIImage(systemName: "calendar")!,
+                                                                unselectedImage: ImageLiterals.icnDailyHappicOff,
+                                                                selectedImage: ImageLiterals.icnDailyHappicOn,
                                                                 rootViewController: HaruHappicController())
         
         let createContentsController = templateNavigationController(title: "",
-                                                                    unselectedImage: UIImage(systemName: "plus.circle")!,
-                                                                    selectedImage: UIImage(systemName: "plus.circle")!,
+                                                                    unselectedImage: ImageLiterals.btnPlus,
+                                                                    selectedImage: ImageLiterals.btnPlus,
                                                                     rootViewController: CreateContentsController())
         
         let happicReportController = templateNavigationController(title: "해픽레포트",
-                                                                  unselectedImage: UIImage(systemName: "doc")!,
-                                                                  selectedImage: UIImage(systemName: "doc")!,
+                                                                  unselectedImage: ImageLiterals.icnHappicReportOff,
+                                                                  selectedImage: ImageLiterals.icnHappicReportOn,
                                                                   rootViewController: HappicReportController())
         
         let settingController = templateNavigationController(title: "설정",
-                                                             unselectedImage: UIImage(systemName: "gearshape")!,
-                                                             selectedImage: UIImage(systemName: "gearshape")!,
+                                                             unselectedImage: ImageLiterals.icnSettingOff,
+                                                             selectedImage: ImageLiterals.icnSettingOn,
                                                              rootViewController: SettingController())
         
         viewControllers = [homeController, haruHappicController, createContentsController, happicReportController, settingController]
