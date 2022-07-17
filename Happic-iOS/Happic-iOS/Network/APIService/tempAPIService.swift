7108e19485f38a438055a611b4eecf68239d661b
Sejin Lee <lsj8706@naver.com>
2022-07-18 03:25:49 +0900

    [Add] 네트워크 기초 세팅 파일 추가

--- Happic-iOS/Happic-iOS/Network/APIService/tempAPIService.swift
+++ Happic-iOS/Happic-iOS/Network/APIService/tempAPIService.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import Alamofire
+
+// Service code 예시
+
+// struct UserService {
+//    static let shared = UserService()
+//
+//    private init() {}
+//
+//        // 회원가입
+//    func signUp(name: String, email: String, password: String, completion: @escaping (NetworkResult<Any>) -> Void) {
+//        let url = APIConstants.signURL
+//        let header: HTTPHeaders = ["Content-Type" : "application/json"]
+//        let body: Parameters = [
+//            "name": name,
+//            "email": email,
+//            "password": password
+//        ]
+//
+//        // Request 생성
+//                // get통신에서는 encoding에 URLEncoding.default 사용
+//        let dataRequest = AF.request(url, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header)
+//
+//        // Request 시작
+//        dataRequest.responseData { response in
+//            switch response.result {
+//            // 성공 시 상태코드와 데이터(value) 수신
+//            case .success:
+//                guard let statusCode = response.response?.statusCode else { return }
+//                guard let value = response.value else { return }
+//
+//                let networkResult = NetworkHelper.parseJSON(by: statusCode, data: value, type: SignUpData.self)
+//                completion(networkResult)
+//
+//            // 실패 시 networkFail(통신 실패)신호 전달
+//            case .failure:
+//                completion(.networkFail)
+//            }
+//        }
+//    }
+// }
---
+++ Happic-iOS/Happic-iOS/Network/Foundation/NetworkResult.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkResult.swift
+//  Happic-iOS
+//
+//  Created by sejin on 2022/07/18.
+//
+
+import Foundation
+
+enum NetworkResult<T> {
+    case success(T) //서버 통신 성공했을 때
+    case requestErr(T) // 요청 에러 발생했을 때
+    case pathErr(T) // 경로 에러 발생했을 때
+    case serverErr // 서버의 내부 에러가 발생했을 때
+    case networkFail // 네트워크 연결 실패했을 때
+}
