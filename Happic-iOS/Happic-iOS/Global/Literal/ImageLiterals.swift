4cb82d24772511c39b74b2d3c5c662fab1adbeae
devxsby <devxsby@gmail.com>
2022-07-18 00:24:26 +0900

    [Feat] ImageLiterals 추가

--- Happic-iOS/Happic-iOS/Global/Literal/ImageLiterals.swift
+++ Happic-iOS/Happic-iOS/Global/Literal/ImageLiterals.swift
@@ -12,19 +12,28 @@ enum ImageLiterals {
     static var bgSplash: UIImage { .load(named: "bg_splash") }
     static var hpImageSplash: UIImage { .load(named: "hp_image_splash") }
     
-    static var icnKakao: UIImage { .load(named: "hp_ic_kakao")}
-    static var icnAlert: UIImage { .load(named: "hp_ic_alert")}
-    static var icnTrash: UIImage { .load(named: "hp_ic_trash")}
-    static var icnInfo: UIImage { .load(named: "hp_ic_info")}
-    static var icnPlus: UIImage { .load(named: "hp_ic_plus")}
-    static var icnLeft: UIImage { .load(named: "hp_ic_left")}
-    static var icnRight: UIImage { .load(named: "hp_ic_right")}
-    static var icnUp: UIImage { .load(named: "hp_ic_up")}
-    static var icnDown: UIImage { .load(named: "hp_ic_down")}
-    static var icnUpMini: UIImage { .load(named: "hp_ic_up_mini")}
-    static var icnDownMini: UIImage { .load(named: "hp_ic_down_mini")}
+    static var icnKakao: UIImage { .load(named: "hp_ic_kakao") }
+    static var icnAlert: UIImage { .load(named: "hp_ic_alert") }
+    static var icnCancel: UIImage { .load(named: "hp_ic_cancel") }
+    static var icnTrash: UIImage { .load(named: "hp_ic_trash") }
+    static var icnInfo: UIImage { .load(named: "hp_ic_info") }
+    static var icnPlus: UIImage { .load(named: "hp_ic_plus") }
+    static var icnLeft: UIImage { .load(named: "hp_ic_left") }
+    static var icnRight: UIImage { .load(named: "hp_ic_right") }
+    static var icnUp: UIImage { .load(named: "hp_ic_up") }
+    static var icnDown: UIImage { .load(named: "hp_ic_down") }
+    static var icnUpMini: UIImage { .load(named: "hp_ic_up_mini") }
+    static var icnDownMini: UIImage { .load(named: "hp_ic_down_mini") }
     
+    static var icnHomeOff: UIImage { .load(named: "hp_ic_home_off") }
+    static var icnHomeOn: UIImage { .load(named: "hp_ic_home_on") }
+    static var icnDailyHappicOff: UIImage { .load(named: "hp_ic_dh_off") }
+    static var icnDailyHappicOn: UIImage { .load(named: "hp_ic_dh_on") }
     static var btnPlus: UIImage { .load(named: "hp_btn_plus") }
+    static var icnHappicReportOff: UIImage { .load(named: "hp_ic_hr_off") }
+    static var icnHappicReportOn: UIImage { .load(named: "hp_ic_hr_on") }
+    static var icnSettingOff: UIImage { .load(named: "hp_ic_set_off") }
+    static var icnSettingOn: UIImage { .load(named: "hp_ic_set_on") }
     
     static var imageCloudLv1: UIImage { .load(named: "hp_img_c1") }
     static var imageCloudLv2: UIImage { .load(named: "hp_img_c2") }
@@ -42,10 +51,10 @@ enum ImageLiterals {
     static var imageCapsule: UIImage { .load(named: "image_capsule") } /// 변경 예정
 
     // 샘플 이미지
-    static var imageDailySample1: UIImage { .load(named: "image_daily_sample1")}
-    static var imageDailySample2: UIImage { .load(named: "image_daily_sample2")}
-    static var imageDailySample3: UIImage { .load(named: "image_daily_sample3")}
-    static var imageDailySample4: UIImage { .load(named: "image_daily_sample4")}
+    static var imageDailySample1: UIImage { .load(named: "image_daily_sample1") }
+    static var imageDailySample2: UIImage { .load(named: "image_daily_sample2") }
+    static var imageDailySample3: UIImage { .load(named: "image_daily_sample3") }
+    static var imageDailySample4: UIImage { .load(named: "image_daily_sample4") }
 }
 
 extension UIImage {
