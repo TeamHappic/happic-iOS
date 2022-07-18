6480567cf95b3be77e535dbb34699ca63706a3bd
devxsby <devxsby@gmail.com>
2022-07-19 03:23:52 +0900

    [Add] HaruHappicDetailController 파일 생성

---
+++ Happic-iOS/Happic-iOS/Screens/HaruHappic/Controller/HaruHappicDetailController.swift
@@ -0,0 +1,16 @@
+//
+//  HaruHappicDetailController.swift
+//  Happic-iOS
+//
+//  Created by devxsby on 2022/07/19.
+//
+
+import UIKit
+
+class HaruHappicDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
