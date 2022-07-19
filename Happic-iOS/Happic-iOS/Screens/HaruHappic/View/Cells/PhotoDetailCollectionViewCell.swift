9bef2f79cb9048b7cc88d2f5c5e75c732e9fe418
devxsby <devxsby@gmail.com>
2022-07-19 18:11:09 +0900

    [Feat] PhotoDetailCollectionViewCell 추가

--- Happic-iOS/Happic-iOS/Screens/HaruHappic/View/Cells/PhotoDetailCollectionViewCell.swift
+++ Happic-iOS/Happic-iOS/Screens/HaruHappic/View/Cells/PhotoDetailCollectionViewCell.swift
@@ -9,4 +9,31 @@ import UIKit
 
 class PhotoDetailCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - UI
+    private lazy var dailyImage = UIImageView().then {
+        $0.image = ImageLiterals.imageDailySample4
+        $0.layer.cornerRadius = 12
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+    }
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    private func configureUI() {
+        
+        addSubview(dailyImage)
+        
+        dailyImage.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }    
 }
