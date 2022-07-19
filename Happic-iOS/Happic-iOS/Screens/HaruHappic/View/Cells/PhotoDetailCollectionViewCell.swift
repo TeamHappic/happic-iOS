fab9ac81dfb17ec61655fc7096ab613465b458ad
devxsby <devxsby@gmail.com>
2022-07-19 18:01:11 +0900

    [Add] PhotoDetailCollectionViewCell 파일 생성

---
+++ Happic-iOS/Happic-iOS/Screens/HaruHappic/View/Cells/PhotoDetailCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoDetailCollectionViewCell.swift
+//  Happic-iOS
+//
+//  Created by devxsby on 2022/07/19.
+//
+
+import UIKit
+
+class PhotoDetailCollectionViewCell: UICollectionViewCell {
+    
+}
