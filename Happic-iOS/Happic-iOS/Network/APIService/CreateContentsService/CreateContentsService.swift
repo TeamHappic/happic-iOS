3cb1744f7c5899cb763f8feae76c9ade8fb75bee
devxsby <devxsby@gmail.com>
2022-07-21 22:37:11 +0900

    [Docs] 주석 수정

--- Happic-iOS/Happic-iOS/Network/APIService/CreateContentsService/CreateContentsService.swift
+++ Happic-iOS/Happic-iOS/Network/APIService/CreateContentsService/CreateContentsService.swift
@@ -78,7 +78,7 @@ struct CreateContentsService {
         }
     }
     
-    // 하루해픽 게시글 삭제
+    /// 하루해픽 게시글 삭제
     func deleteHaruHappic(filmId: String, completion: @escaping (NetworkResult<Any>) -> Void) {
         let url = APIConstants.createContentsURL
         let header: HTTPHeaders = ["Content-Type": "application/json", "x-auth-token": "jwt token"]
