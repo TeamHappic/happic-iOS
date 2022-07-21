5e3bd2ae46ecfdec93fe7099fd5566b458ea5151
devxsby <devxsby@gmail.com>
2022-07-22 08:21:38 +0900

    [Chore] createHaruHappic header 수정

--- Happic-iOS/Happic-iOS/Network/APIService/CreateContentsService/CreateContentsService.swift
+++ Happic-iOS/Happic-iOS/Network/APIService/CreateContentsService/CreateContentsService.swift
@@ -78,7 +78,7 @@ struct CreateContentsService {
     /// 하루해픽 게시글 생성
     func createHaruHappic(photo: String, when: String, place: String, who: String, what: String, completion: @escaping (NetworkResult<Any>) -> Void) {
         let url = APIConstants.createContentsURL
-        let header: HTTPHeaders = ["Content-Type": "application/json", "x-auth-token": "jwt token"]
+        let header: HTTPHeaders = ["Content-Type": "application/json", "Authorization": "Bearer " + UserDefaults.tempJWT]
         let body: Parameters = ["photo": photo,
                                 "when": when,
                                 "where": place,
