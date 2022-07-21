2ba9ed01c576d4de5a2111e56a4a01cb96240235
devxsby <devxsby@gmail.com>
2022-07-21 21:15:54 +0900

    [Chore] CreateContentsModel 수정

--- Happic-iOS/Happic-iOS/Network/Model/CreateContentsModel/CreateContentsModel.swift
+++ Happic-iOS/Happic-iOS/Network/Model/CreateContentsModel/CreateContentsModel.swift
@@ -9,12 +9,6 @@ import Foundation
 
 // MARK: - CreateContentsModel
 struct CreateContentsModel: Codable {
-    let photo: String
-    let when, place, who, what: String
-
-    enum CodingKeys: String, CodingKey {
-        case photo, when, who, what
-        case place = "where"
-    }
+    let id: String
 }
 
