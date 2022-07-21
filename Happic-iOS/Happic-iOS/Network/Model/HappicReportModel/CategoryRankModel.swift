b7decabd40ccfc6c4f046217cfe0658a9451e983
Sejin Lee <lsj8706@naver.com>
2022-07-22 05:53:22 +0900

    [Feat] 카테고리 랭크 모델 추가

---
+++ Happic-iOS/Happic-iOS/Network/Model/HappicReportModel/CategoryRankModel.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryRankModel.swift
+//  Happic-iOS
+//
+//  Created by sejin on 2022/07/22.
+//
+
+import Foundation
+
+struct CategoryRankModel: Codable {
+    let ranks: [KeywordModel]
+}
