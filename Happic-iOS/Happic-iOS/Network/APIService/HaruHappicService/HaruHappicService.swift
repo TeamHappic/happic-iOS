7b95ab462f77909eef5496ed5244003fe6d182e4
devxsby <devxsby@gmail.com>
2022-07-21 16:03:30 +0900

    [Add] HaruHappicService 파일 생성

---
+++ Happic-iOS/Happic-iOS/Network/APIService/HaruHappicService/HaruHappicService.swift
@@ -0,0 +1,21 @@
+//
+//  HaruHappicService.swift
+//  Happic-iOS
+//
+//  Created by devxsby on 2022/07/21.
+//
+
+import Foundation
+import Alamofire
+
+struct HaruHappicService {
+    static let shared = HaruHappicService()
+    
+    private init() {}
+    
+    // MARK: - 하루해픽 사진 뷰 전체 조회
+    
+    func getHaruHappicPhotoView() {
+        
+    }
+}
