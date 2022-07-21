bfe227c3f37067a0a695753da66ddc979fe6726f
ZaidKang <parandl@naver.com>
2022-07-21 23:17:06 +0900

    [Feat] 캐릭터 소개뷰 -> 캐릭터 이름뷰 연결 및 Flag 데이터 전달

---
+++ Happic-iOS/Happic-iOS/Screens/CreateCharacter/Controller/CharacterNameViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class CharacterNameViewController: UIViewController{
+
+    var flag: Int = 0 {
+        didSet {
+            print(flag)
+        }
+    }
+
+    override func viewDidLoad() {
+          super.viewDidLoad()
+    }
+    
+    
+    
+}
