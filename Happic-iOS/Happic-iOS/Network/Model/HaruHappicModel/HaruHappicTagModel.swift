df41a0738a756b2f178da89caf584fc640ff6790
devxsby <devxsby@gmail.com>
2022-07-21 16:44:42 +0900

    [Feat] HaruHappicTagModel 작성

--- Happic-iOS/Happic-iOS/Network/Model/HaruHappicModel/HaruHappicTagModel.swift
+++ Happic-iOS/Happic-iOS/Network/Model/HaruHappicModel/HaruHappicTagModel.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct HaruHappicTagModel {
+    let id, date, when, place, who, what: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, date, when, who, what
+        case place = "where"
+    }
+}
