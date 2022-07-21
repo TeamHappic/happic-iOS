00ab1b9e34fb5863197a93f680f6ad0cc6cdda71
devxsby <devxsby@gmail.com>
2022-07-21 21:07:18 +0900

    [Feat] postHaruHappic 추가

--- Happic-iOS/Happic-iOS/Network/APIService/CreateContentsService/CreateContentsService.swift
+++ Happic-iOS/Happic-iOS/Network/APIService/CreateContentsService/CreateContentsService.swift
@@ -52,4 +52,28 @@ struct CreateContentsService {
             }
         }
     }
+    
+    // 하루해픽 게시글 생성
+    func postHaruHappic(photo: String, when: String, place: String, who: String, what: String, completion: @escaping (NetworkResult<Any>) -> Void) {
+        let url = APIConstants.createContentsURL
+        let header: HTTPHeaders = ["Content-Type": "application/json", "x-auth-token": "jwt token"]
+        let body: Parameters = ["photo": photo,
+                                "when": when,
+                                "where": place,
+                                "who": who,
+                                "what": what]
+        let dataRequest = AF.request(url, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header)
+        
+        dataRequest.responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let value = response.value else { return }
+                let networkResult = NetworkHelper.parseJSON(by: statusCode, data: value, type: CreateContentsModel.self)
+                completion(networkResult)
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
 }
