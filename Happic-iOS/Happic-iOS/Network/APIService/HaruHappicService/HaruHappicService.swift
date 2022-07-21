856e58afe8638a1d943b4fb67cf7e54d03cdfed0
devxsby <devxsby@gmail.com>
2022-07-21 16:27:07 +0900

    [Feat] getHaruHappicPhotoView 추가

--- Happic-iOS/Happic-iOS/Network/APIService/HaruHappicService/HaruHappicService.swift
+++ Happic-iOS/Happic-iOS/Network/APIService/HaruHappicService/HaruHappicService.swift
@@ -14,8 +14,26 @@ struct HaruHappicService {
     private init() {}
     
     // MARK: - 하루해픽 사진 뷰 전체 조회
-    
-    func getHaruHappicPhotoView() {
+    func getHaruHappicPhotoView(year: Int, month: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+        let url = APIConstants.haruHappicPhotoURL
+        let header: HTTPHeaders = ["Content-Type": "application/json", "x-auth-token": "jwt token"]
+        let parameters: Parameters = [
+            "year": year,
+            "month": month
+        ]
         
+        let dataRequest = AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: header)
+        
+        dataRequest.responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let value = response.value else { return }
+                let networkResult = NetworkHelper.parseJSON(by: statusCode, data: value, type: [HaruHappicPhotoModel].self)
+                completion(networkResult)
+            case .failure:
+                completion(.networkFail)
+            }
+        }
     }
 }
