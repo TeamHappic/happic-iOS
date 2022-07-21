d0c0edc8efb312bd3ca6ea3c1624e84d156fca28
devxsby <devxsby@gmail.com>
2022-07-21 20:46:45 +0900

    [Feat] getRecommendTag 추가

--- Happic-iOS/Happic-iOS/Network/APIService/CreateContentsService/CreateContentsService.swift
+++ Happic-iOS/Happic-iOS/Network/APIService/CreateContentsService/CreateContentsService.swift
@@ -32,4 +32,24 @@ struct CreateContentsService {
             }
         }
     }
+    
+    /// 게시글 생성 시 최다 태그 추천
+    func getRecommendTag(completion: @escaping (NetworkResult<Any>) -> Void) {
+    let url = APIConstants.isPostedURL
+    let header: HTTPHeaders = ["Content-Type": "application/json", "x-auth-token": "jwt token"]
+    
+    let dataRequest = AF.request(url, method: .get, encoding: URLEncoding.default, headers: header)
+    
+    dataRequest.responseData { response in
+        switch response.result {
+        case .success:
+            guard let statusCode = response.response?.statusCode else { return }
+            guard let value = response.value else { return }
+            let networkResult = NetworkHelper.parseJSON(by: statusCode, data: value, type: RecommendTagModel.self)
+            completion(networkResult)
+        case .failure:
+            completion(.networkFail)
+        }
+    }
+}
 }
