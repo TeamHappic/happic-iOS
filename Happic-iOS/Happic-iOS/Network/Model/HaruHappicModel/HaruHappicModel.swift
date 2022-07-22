af3e39c671d903a4492ae55c580acb231c270d98
devxsby <devxsby@gmail.com>
2022-07-23 04:47:50 +0900

    [Feat] HaruHappicModel에 dayOfWeek 추가

--- Happic-iOS/Happic-iOS/Network/Model/HaruHappicModel/HaruHappicModel.swift
+++ Happic-iOS/Happic-iOS/Network/Model/HaruHappicModel/HaruHappicModel.swift
@@ -9,11 +9,11 @@ import Foundation
 
 // MARK: - HaruHappicModel
 struct HaruHappicModel: Codable {
-    let id, photo, thumbnail, place, who, what: String
+    let id, photo, thumbnail, place, who, what, dayOfWeek: String
     let day, when: Int
 
     enum CodingKeys: String, CodingKey {
-        case id, day, photo, thumbnail, when, who, what
+        case id, day, photo, thumbnail, when, who, what, dayOfWeek
         case place = "where"
     }
 }
