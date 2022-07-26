756e419e4a315adb5a94fe8afcf7a07e6172badc
Sejin Lee <lsj8706@naver.com>
2022-07-27 01:51:59 +0900

    [Feat] UserDefaultWrapper 생성

---
+++ Happic-iOS/Happic-iOS/Global/Util/UserDefaultWrapper.swift
@@ -0,0 +1,28 @@
+//
+//  UserDefaultWrapper.swift
+//  Happic-iOS
+//
+//  Created by sejin on 2022/07/27.
+//
+
+import Foundation
+
+@propertyWrapper struct UserDefaultWrapper<T> {
+    
+    var wrappedValue: T? {
+        get {
+            return UserDefaults.standard.object(forKey: self.key) as? T
+        }
+        
+        set {
+            if newValue == nil { UserDefaults.standard.removeObject(forKey: key) }
+            else { UserDefaults.standard.setValue(newValue, forKey: key) }
+        }
+    }
+    
+    private let key: String
+    
+    init(key: String) {
+        self.key = key
+    }
+}
---
+++ Happic-iOS/Happic-iOS/Global/Util/UserManager.swift
@@ -0,0 +1,33 @@
+//
+//  UserManager.swift
+//  Happic-iOS
+//
+//  Created by sejin on 2022/07/27.
+//
+
+import Foundation
+
+final class UserManager {
+    
+    // MARK: - Properties
+    
+    static let shared = UserManager()
+    private(set) var currentLoginStatus: Bool?
+    
+    @UserDefaultWrapper<String>(key: "jwtToken") private(set) var jwtToken
+    
+    var hasJwtToken: Bool { return self.jwtToken != nil }
+
+    var isLogin: Bool { return self.currentLoginStatus ?? false }
+    var getJwtToken: String { return self.jwtToken ?? "" }
+
+    // MARK: - Life Cycles
+    
+    private init() {}
+    
+    // MARK: - Fuctions
+    
+    func setSocialToken(token: String) {
+        self.jwtToken = token
+    }
+}
